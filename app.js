95e413d1b620f8ddd90a707a91f1ca921f59f6c1
Urielstam <urielstamelman@gmail.com>
2023-05-08 17:47:43 +0300

    Add node() and tree() facts

---
+++ app.js
@@ -0,0 +1,21 @@
+const node = (data, left, right) => {
+    return {
+        data,
+        left,
+        right
+    }
+}
+
+const tree = ((arr) => {
+    let root;
+    let array = arr;
+
+    return {
+        root,
+        array
+    }
+})();
+
+
+
+
