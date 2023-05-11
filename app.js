692a0ca672b05ec14d655131538384285dc6de36
Urielstam <urielstamelman@gmail.com>
2023-05-11 21:22:42 +0300

    Add insert() to insert value to tree accordinly

--- app.js
+++ app.js
@@ -24,9 +24,26 @@ const tree = (arr) => {
     let sortedArr = [...new Set(arr)].sort((a, b) => a -b);
     let root = buildTree(sortedArr);
 
+    // compare val to root 
+      // if less move left
+      // else move right
+    //continue until hit leaf node -> than create leaf node accordingly
+    const insert = (value, rootNode = root) => {
+      if(rootNode === null) return node(value);
+
+      if(value < rootNode.data) {
+        rootNode.left = insert(value, rootNode.left);
+      } else {
+        rootNode.right = insert(value, rootNode.right);
+      }
+      return rootNode
+
+    }
+
     return {
         root,
-        sortedArr
+        sortedArr,
+        insert
     }
 };
 
@@ -46,6 +63,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 
 let newTree = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(newTree.root.data);
+console.log(newTree.root);
+newTree.insert(6)
 prettyPrint(newTree.root);
 
